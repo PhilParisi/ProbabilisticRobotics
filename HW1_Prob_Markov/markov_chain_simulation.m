1cc15e7cdad8091465f5349a4ade7587f0fb8ea3
PhilParisi <philsbeginnercode@gmail.com>
2023-02-01 13:38:37 -0500

    updated markov chain assignment, fully working with experimental matching theoretical

--- HW1_Prob_Markov/markov_chain_simulation.m
+++ HW1_Prob_Markov/markov_chain_simulation.m
@@ -7,8 +7,8 @@ clc, clearvars
 tic
 
 % number of days to simulate
-days = 20;
-trials = 100000;
+days = 2000;
+trials = 10000;
 
 % Markov Chain (embedded into generate_weather.m)
 prob = [0.8 0.2 0.0;            % sunny
@@ -47,3 +47,28 @@ disp(tb)
 
 toc
 disp('done!')
+
+
+%% Steady State Calcs
+% calculating the steady state of a markov chain
+% create a system of equations
+
+% Eqn 1, all probabilities in steady state must equal 1
+    % p1 + p2 + p3
+
+% Eqn 2, p1 must equal all other steady states entering state 1
+    % p1 = p1(0.8) + p2(0.4) + p3(0.2)
+
+% Eqn 3, p2 must equal all other steady states entering state 2
+    % p2 = p1(0.2) + p2(0.4) + p3 (0.6)
+
+% could also make an equation 4, but we now have 3 eqns and 3 unkowns
+
+% solve using reduced row echelon form, linear algebra
+
+A = [ 1.0 1.0 1.0 1.0;
+     -0.2 0.4 0.2 0.0;
+     0.2 -0.6 0.6 0.0];
+
+rref(A) %final column shows [p1; p2; p3]
+
